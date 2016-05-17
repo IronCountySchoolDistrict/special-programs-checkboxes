9809ece46900169297ad09ea03df166f983c1ee6
Basiczombie <issacgable@gmail.com>
2016-05-17 15:18:05 -0600

    Fixed business logic and style tags

--- src/scripts/special-programs-checkboxes/js/spcheckbox.js
+++ src/scripts/special-programs-checkboxes/js/spcheckbox.js
@@ -1,20 +1,22 @@
 (function ($){
   //init a put request
-  function _ajax_request(url, data, callback, method) {
+  function _ajax_request(url, data, callback, dataType, contentType, method) {
       return jQuery.ajax({
           url: url,
           type: method,
           data: data,
-          success: callback
+          success: callback,
+          dataType: dataType,
+          contentType: contentType
       });
   }
   jQuery.extend({
-      put: function(url, data, callback) {
-          return _ajax_request(url, data, callback, 'PUT');
+      put: function(url, data, callback, dataType, contentType) {
+          return _ajax_request(url, data, callback, dataType, contentType, 'PUT');
   }});
   //gets state_student_record
   function get_state_student_record(){
-    return $.get("/ws/schema/table/S_UT_STU_X/"+psData.studentDcid+"?projection=*").done(
+    return $.getJSON("/ws/schema/table/S_UT_STU_X/"+psData.studentDcid+"?projection=*").done(
       function (data){
         return data;
       }
@@ -28,22 +30,20 @@
     $(".box-round").before($("#spcheckbox").html());
 
     $("#sped").on("change", function(){
-      var value = $("#sped").val();
+      var value = $("#sped").is(":checked") ? "1" : "0";
       var record = {
-        "id": state_student_record.id,
+        "id": JSON.parse(state_student_record.responseText).id,
         "name": "S_UT_STU_X",
          "tables":{
         	  "S_UT_STU_X":{
-        		 "studentsdcid": psData.studentDcid,
         		 "special_ed_indicator": value
         	  }
          }
       }
-      console.log(record);
 
-      $.put("/ws/schema/table/S_UT_STU_X/"+psData.studentDcid, record, function(data){
-        console.log(data);
-      }, "json");
+      $.put("/ws/schema/table/S_UT_STU_X/"+psData.studentDcid, JSON.stringify(record), function(data){
+        console.log("Changes have been saved!");
+      }, "json", "application/json; charset=utf-8");
 
     })
   });
