511b2c27dfdf76104700a7f5735236cbac746b84
Basiczombie <issacgable@gmail.com>
2016-05-17 10:20:50 -0600

    Prototype Complete

---
+++ src/scripts/special-programs-checkboxes/js/spcheckbox.js
@@ -0,0 +1,49 @@
+(function ($){
+  //init a put request
+  function _ajax_request(url, data, callback, method) {
+      return jQuery.ajax({
+          url: url,
+          type: method,
+          data: data,
+          success: callback
+      });
+  }
+  jQuery.extend({
+      put: function(url, data, callback) {
+          return _ajax_request(url, data, callback, 'PUT');
+  }});
+  //gets state_student_record
+  function get_state_student_record(){
+    return $.get("/ws/schema/table/S_UT_STU_X/"+psData.studentDcid+"?projection=*").done(
+      function (data){
+        return data;
+      }
+    );
+  }
+
+  var state_student_record = get_state_student_record();
+
+  $("#sped").on("change", function(){
+    var value = $("#sped").val();
+    var record = {
+       "tables":{
+      	  "S_UT_STU_X":{
+      		 "studentsdcid": psData.studentDcid,
+      		 "special_ed_indicator": value
+      	  }
+       }
+    }
+    if(!state_student_record){
+      $.post("/ws/schema/table/S_UT_STU_X", record, function(data){
+        console.log(data);
+      }, "json");
+    }else {
+      record.id = state_student_record;
+      record.name = "S_UT_STU_X";
+      $.put("/ws/schema/table/S_UT_STU_X/"+psData.studentDcid, record, function(data){
+        console.log(data);
+      }, "json");
+    }
+
+  })
+})($j);
