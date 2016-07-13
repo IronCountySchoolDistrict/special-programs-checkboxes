ffd3931c92b4837c712f920de91dddb7e7339076
basiczombie <issacgable@gmail.com>
2016-07-13 07:27:42 -0600

    finsihed refactoring

--- src/scripts/special-programs-checkboxes/js/spcheckbox.js
+++ src/scripts/special-programs-checkboxes/js/spcheckbox.js
@@ -1,87 +1,85 @@
 (function ($){
-  //init a put request
-  function _ajax_request(url, data, callback, dataType, contentType, method) {
-      return jQuery.ajax({
-          url: url,
-          type: method,
-          data: data,
-          success: callback,
-          dataType: dataType,
-          contentType: contentType
-      });
+  // Get State Student Record
+  var get_ssr = fetch(`/ws/schema/table/S_UT_STU_X/${psData.studentDcid}?projection=*`, {
+    method: 'GET',
+    credentials: 'same-origin',
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => response.json())
+  // Get ELL Exteneded Table
+  var get_ell = fetch(`/ws/schema/table/U_DEF_EXT_STUDENTS/${psData.studentDcid}?projection=*`, {
+    method: 'GET',
+    credentials: 'same-origin',
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => response.json())
+  // Get HTML page fragment to be loaded
+  var page_fragment = fetch('/scripts/special-programs-checkboxes/html/spcheckboxes.html')
+    .then(response => response.text())
+
+  function put(loc, data){
+    fetch(loc, {
+      method: 'PUT',
+      credentials: 'same-origin',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
   }
-  jQuery.extend({
-      put: function(url, data, callback, dataType, contentType) {
-          return _ajax_request(url, data, callback, dataType, contentType, 'PUT');
-  }});
 
   $(document).ready( function(){
+    // Sets the location of program tables
+    var sped_loc = "/ws/schema/table/S_UT_STU_X/"+psData.studentDcid;
+    var ell_loc = "/ws/schema/table/U_DEF_EXT_STUDENTS/"+psData.studentDcid;
 
-    var get_ssr = fetch(`/ws/schema/table/S_UT_STU_X/${psData.studentDcid}?projection=*`, {
-      method: 'GET',
-      credentials: 'same-origin',
-      headers: {
-        'Accept': 'application/json'
-      }
-    }).then(response => response.json())
-
-    var get_ell = fetch(`/ws/schema/table/U_DEF_EXT_STUDENTS/${psData.studentDcid}?projection=*`, {
-      method: 'GET',
-      credentials: 'same-origin',
-      headers: {
-        'Accept': 'application/json'
-      }
-    }).then(response => response.json())
-
-    var page_fragment = fetch('/scripts/special-programs-checkboxes/html/spcheckboxes.html')
-      .then(response => response.text())
-
+    // Resolve all the Fetch var
     Promise.all([get_ssr, get_ell, page_fragment])
       .then(function([state_student_record, ell, body]) {
+        // Sets the current value of the program indicator
+        var sped_check = state_student_record.tables.s_ut_stu_x.special_ed_indicator;
+        var ell_check = ell.tables.u_def_ext_students.ell_indicator;
+        // Insert above the table in the HTML page
         $(".box-round").before(body);
-          // Special Education checkbox control
-          if(state_student_record.tables.s_ut_stu_x.special_ed_indicator === "true"){
-            $("#sped").prop('checked', true);
+        //Checks to see if sped has already been checked and sets the checkbox
+        if(sped_check === "true"){
+          $("#sped").prop('checked', true);
+        }
+        //Checks to see if ell has already been checked and sets the checkbox
+        if(ell_check === "true"){
+          $("#ell").prop('checked', true);
+        }
+        // Special Education checkbox control
+        $("#sped").on("change", function(){
+          var value = $("#sped").is(":checked") ? "1" : "0";
+          var record = {
+            "id": state_student_record.id,
+            "name": "S_UT_STU_X",
+             "tables":{
+            	  "S_UT_STU_X":{
+            		 "special_ed_indicator": value
+            	  }
+             }
           }
-          if(ell.tables.u_def_ext_students.ell_indicator === "true"){
-            $("#ell").prop('checked', true);
-          }
-
-          $("#sped").on("change", function(){
-            var value = $("#sped").is(":checked") ? "1" : "0";
-            var record = {
-              "id": state_student_record.id,
-              "name": "S_UT_STU_X",
-               "tables":{
-              	  "S_UT_STU_X":{
-              		 "special_ed_indicator": value
-              	  }
-               }
-            }
-
-            $.put("/ws/schema/table/S_UT_STU_X/"+psData.studentDcid, JSON.stringify(record), function(data){
-              console.log("Changes have been saved!");
-            }, "json", "application/json; charset=utf-8");
-
-          })
-          // ELL checkbox control
-          $("#ell").on("change", function(){
-            var value = $("#ell").is(":checked") ? "1" : "0";
-            var record = {
-              "id": ell.id,
-              "name": "U_DEF_EXT_STUDENTS",
-               "tables":{
-              	  "U_DEF_EXT_STUDENTS":{
-              		 "ell_indicator": value
-              	  }
-               }
-            }
-
-            $.put("/ws/schema/table/U_DEF_EXT_STUDENTS/"+psData.studentDcid, JSON.stringify(record), function(data){
-              console.log("Changes have been saved!");
-            }, "json", "application/json; charset=utf-8");
-
-          })
+          put(sped_loc, record)
         })
-      })
+        // ELL checkbox control
+        $("#ell").on("change", function(){
+          var value = $("#ell").is(":checked") ? "1" : "0";
+          var record = {
+            "id": ell.id,
+            "name": "U_DEF_EXT_STUDENTS",
+             "tables":{
+            	  "U_DEF_EXT_STUDENTS":{
+            		 "ell_indicator": value
+            	  }
+             }
+          }
+          put(ell_loc, record)
+        })
+    })
+  })
 })($j);
